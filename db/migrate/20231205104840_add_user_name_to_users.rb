b78ad6074bd7f5b741e5fb7d46f4e0a9481499a0
Constantin Lerner <colerner@me.com>
2023-12-05 11:57:15 +0100

    started working on the user model

---
+++ db/migrate/20231205104840_add_user_name_to_users.rb
@@ -0,0 +1,4 @@
+class AddUserNameToUsers < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_100909) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_104840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

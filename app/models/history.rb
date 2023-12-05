d6085a079a77a83c89d5bd39e24f9f8e597bdfa2
Bruno Fernandes <bruno.m.c.fer@gmail.com>
2023-12-05 15:05:02 +0000

    more changes

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,5 @@
 class History < ApplicationRecord
   belongs_to :sport
 
+  validates :invented, :creator, :country_of_origin, :most_successful_teams, :milestones, presence: true
 end
--- app/models/overview.rb
+++ app/models/overview.rb
@@ -1,4 +1,5 @@
 class Overview < ApplicationRecord
   belongs_to :sport
 
+  validates :short_description, :game_objective, :governing_body_url, presence: true
 end

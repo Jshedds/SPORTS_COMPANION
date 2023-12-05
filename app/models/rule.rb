09248478c3ca680261556910dadf6c547452b57b
Thomas Furio <tommyfurio@gmail.com>
2023-12-05 15:12:45 +0100

    Validations and models complete

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,5 +3,6 @@ class Rule < ApplicationRecord
 
   # rubocop:disable Layout/LineLength
   delegate :game_duration, :scoring, :player_per_team, :referees, :most_important_rules, :field_size, :field_type, to: :sport
+  validates :game_duration, :scoring, :player_per_team, :referees, :most_important_rules, :field_size, :field_type, presence: true
   # rubocop:enable Layout/LineLength
 end

4ae78a8451566b6f2e094ef0a4a7d1071a0965b2
Constantin Lerner <colerner@me.com>
2023-12-06 19:02:59 +0100

    added pundit to the messages model and fixed some style errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,18 @@
 class MessagesController < ApplicationController
-  def create
+  def create # rubocop:disable Metrics/MethodLength
     @sport = Sport.find(params[:sport_id])
     @chatroom = Chatroom.find_by(name: "#{@sport.name} chatroom")
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
+    authorize @message
     @message.save
-      ChatroomChannel.broadcast_to(
-        @chatroom,
-        render_to_string(partial: "messages/message", locals: { message: @message })
-      )
-      head :ok
+
+    ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "messages/message", locals: { message: @message })
+    )
+    head :ok
   end
 
   private

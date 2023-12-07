46623cde8ed4a39fdeaac1eb52af4b83fc5aad49
Bruno Fernandes <bruno.m.c.fer@gmail.com>
2023-12-07 14:22:08 +0000

    new controller actions

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,9 +1,26 @@
 class HistoriesController < ApplicationController
   def create
+    @history = history.new(history_params)
     authorize @history
+    if @history.save
+      redirect_to @sport
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
     authorize @history
+    if @history.update(history_params)
+      redirect_to @history, notice: "History was succesfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def history_params
+    params.require(@history).permit(:invented, :creator, :country_of_origin, :most_successful_teams, :milestones)
   end
 end
--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,9 +1,26 @@
 class RulesController < ApplicationController
   def create
+    @rule = rule.new(rule_params)
     authorize @rule
+    if @rule.save
+      redirect_to @sport
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
     authorize @rule
+    if @rule.update(rule_params)
+      redirect_to @rule, notice: "Overview was succesfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def rule_params
+    params.require(@rule).permit(:game_duration, :scoring, :player_per_team, :referees, :most_important_rules, :field_size, :field_type)
   end
 end

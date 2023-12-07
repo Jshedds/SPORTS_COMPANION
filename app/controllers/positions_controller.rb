ba54953564df22b6ffd699bea85ed19960e76799
Jamie Sheddan <jsheddan@outlook.com>
2023-12-07 16:24:18 +0000

    continuing show pages

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,18 @@
 class PositionsController < ApplicationController
+  def index
+    @sport = Sport.find(params[:id])
+    @positions = Position.all
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
 end

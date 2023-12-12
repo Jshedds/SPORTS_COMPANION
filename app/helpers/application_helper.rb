903c49059a1d1796894a93e460eb1d7181f995a4
Constantin Lerner <colerner@me.com>
2023-12-12 15:38:58 +0100

    index page 90% done, navbar works for all use cases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def render_icon(icon, classes: nil)
+  def render_icon(icon, classes: "", height: nil, width: nil)
     classes ||= ""
-    render "icons/#{icon}", class: classes
+    render "icons/#{icon}", class: classes, height: height, width: width
   end
 end
